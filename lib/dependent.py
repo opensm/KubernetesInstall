d4d6360a8889977ad73e8d7297d200d7411b8c97
yaoshaoqiang <yaoshaoqiang@zhiyuanhui.com>
2021-01-13 13:22:19 +0800

    修改信息

--- lib/dependent.py
+++ lib/dependent.py
@@ -124,6 +124,10 @@ def get_cluster_list():
 
 @tag_decorator
 def dependent():
+    # check_file = os.path.join(CURRENT_PATH, 'tmp', 'dependent.success')
+    # if os.path.exists(check_file):
+    #     RecodeLog.info("=============已存在完成状态文件，跳过执行dependent部分===============")
+    #     return True
     cluster_list = get_cluster_list()
     for host in cluster_list:
         sftp.host = host
@@ -137,11 +141,18 @@ def dependent():
             remote_dir=REMOTE_TMP_DIR
         )
         sftp.close()
+    # Achieve.touch_achieve(achieve=check_file)
     rsync_host()
+    return True
 
 
 @tag_decorator
 def kernel_update():
+    # RecodeLog.info("=============开始执行kernel_update部分===============")
+    # check_file = os.path.join(TAG_FILE_DIR, 'kernel_update.success')
+    # if os.path.exists(check_file):
+    #     RecodeLog.info("=============已存在完成状态文件，跳过执行kernel_update部分===============")
+    #     return True
     command = "bash {0}".format(os.path.join(
         REMOTE_TMP_DIR, 'upgrade-kernel.sh'
     ))
@@ -152,33 +163,42 @@ def kernel_update():
         sftp.remote_cmd(command=command)
         sftp.close()
         RecodeLog.info(msg="主机:{0},执行成功:{1}".format(host, command))
-    reboot = ""
-    while reboot.upper() not in ['YES', 'NO', 'Y', 'N']:
-        reboot = raw_input("注意！！！！！\nkernel升级完成,是否现在重启集群全部主机？yes/YES or NO/no:")
-        if not isinstance(reboot, str):
-            print("输入类型错误")
-            assert False
-    if reboot.upper() == "YES" or reboot.upper() == "Y":
-        # 其他主机重启
-        for host in cluster_list:
-            if LocalExec.check_ip_same(host, IFNAME):
-                continue
-            sftp.host = host
-            sftp.connect()
-            sftp.remote_cmd(command='reboot')
-            RecodeLog.info(msg="主机:{0},执行成功:reboot".format(host))
-            sftp.close()
-        # 重启本机
-        for host in cluster_list:
-            if not LocalExec.check_ip_same(host, IFNAME):
-                continue
-            LocalExec.cmd(cmd_command="reboot")
-    else:
-        RecodeLog.info(msg="请操作完成之后自行重启集群主机")
+    # Achieve.touch_achieve(achieve=check_file)
+    # reboot = ""
+    # while reboot.upper() not in ['YES', 'NO', 'Y', 'N']:
+    #     reboot = raw_input("注意！！！！！\nkernel升级完成,是否现在重启集群全部主机？yes/YES or NO/no:")
+    #     if not isinstance(reboot, str):
+    #         print("输入类型错误")
+    #         assert False
+    # if reboot.upper() == "YES" or reboot.upper() == "Y":
+    #     # 其他主机重启
+    #     for host in cluster_list:
+    #         if LocalExec.check_ip_same(host, IFNAME):
+    #             continue
+    #         sftp.host = host
+    #         sftp.connect()
+    #         sftp.remote_cmd(command='reboot')
+    #         RecodeLog.info(msg="主机:{0},执行成功:reboot".format(host))
+    #         sftp.close()
+    #     # 重启本机
+    #     for host in cluster_list:
+    #         if not LocalExec.check_ip_same(host, IFNAME):
+    #             continue
+    #         LocalExec.cmd(cmd_command="reboot")
+    # else:
+    #     RecodeLog.info(msg="请操作完成之后自行重启集群主机")
+    #     assert False
+    # Achieve.touch_achieve(achieve=check_file)
+    return True
 
 
 @tag_decorator
 def docker_install():
+    # RecodeLog.info("=============开始执行docker_install部分===============")
+    # check_file = os.path.join(TAG_FILE_DIR, 'docker_install.success')
+    # if os.path.exists(check_file):
+    #     RecodeLog.info("=============已存在完成状态文件，跳过执行docker_install部分===============")
+    #     return True
     cluster_list = get_cluster_list()
     command = "bash {0}".format(
         os.path.join(
@@ -190,6 +210,8 @@ def docker_install():
         sftp.remote_cmd(command=command)
         sftp.close()
         RecodeLog.info(msg="主机:{0},执行成功:{1}".format(host, command))
+
+    # Achieve.touch_achieve(achieve=check_file)
 
 
 def test_ping():
